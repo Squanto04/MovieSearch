b9c1f482b0cc12c6bdd4181e64f3a7362687fbb5
Jordan Lamb <jdawg2329@gmail.com>
2019-10-04 11:07:10 -0600

    Storyboard Complete, View Controller Files added

---
+++ MovieSearch/MovieSearch/View Controllers/MovieListTableViewController.swift
@@ -0,0 +1,30 @@
+//
+//  MovieListTableViewController.swift
+//  MovieSearch
+//
+//  Created by Jordan Lamb on 10/4/19.
+//  Copyright © 2019 Squanto Inc. All rights reserved.
+//
+
+import UIKit
+
+class MovieListTableViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    // MARK: - Table view data source
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        return 0
+    }
+
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
+
+        // Configure the cell...
+
+        return cell
+    }
+}
---
+++ MovieSearch/MovieSearch/Views/MoviesTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  MoviesTableViewCell.swift
+//  MovieSearch
+//
+//  Created by Jordan Lamb on 10/4/19.
+//  Copyright © 2019 Squanto Inc. All rights reserved.
+//
+
+import UIKit
+
+class MoviesTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
